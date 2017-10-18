ed69ea90de61ef30d62c4e30ed1bf08b264f70dc
David Sun <sunweihua@sunweihuadeiMac.local>
2017-10-18 18:30:42 +0800

    update view top

--- src/views/layout/top.blade.php
+++ src/views/layout/top.blade.php
@@ -3,11 +3,9 @@
     <div class="nav-wrapper">
       <a href="https://larashuo.com" class="brand-logo">LaraShuo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="{{url('register')}}">前台注册</a></li>
         <li><a href="{{url('goods')}}">商品管理</a></li>
-        <li><a href="{{url('login')}}">前台登录</a></li>
-        <li><a href="{{url('admin/login')}}">后台登录</a></li>
         <li><a href="https://larashuo.com/order">订阅课程</a></li>
+         <li><a href="https://larashuo.com/gift-card">优惠券</a></li>
       </ul>
     </div>
   </nav>
