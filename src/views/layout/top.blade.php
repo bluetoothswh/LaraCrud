c5b91ad1429086914a4887f0be53b06a56a772b5
David Sun <sunweihua@sunweihuadeiMac.local>
2017-10-18 18:15:27 +0800

    first commit

---
+++ src/views/layout/top.blade.php
@@ -0,0 +1,13 @@
+
+    <nav>
+    <div class="nav-wrapper">
+      <a href="https://larashuo.com" class="brand-logo">LaraShuo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="{{url('register')}}">前台注册</a></li>
+        <li><a href="{{url('goods')}}">商品管理</a></li>
+        <li><a href="{{url('login')}}">前台登录</a></li>
+        <li><a href="{{url('admin/login')}}">后台登录</a></li>
+        <li><a href="https://larashuo.com/order">订阅课程</a></li>
+      </ul>
+    </div>
+  </nav>
